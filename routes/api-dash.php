dceee1c6c0b1b7bd9292147b376823766782e288
adityathok <aditya0thok@gmail.com>
2025-04-17 15:04:03 +0700

    move config to dash

--- routes/api-dash.php
+++ routes/api-dash.php
@@ -1,8 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 use App\Http\Controllers\Dash\MediaController;
+use App\Http\Controllers\Dash\ConfigController;
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::apiResource('dash/media', MediaController::class);
+    Route::apiResources([
+        'users'         => UsersController::class,
+        'dash/media'    => MediaController::class
+    ]);
+
+    Route::put('user/updatepassword/{id}', [UsersController::class, 'updatePassword']);
+    Route::put('user/updateavatar/{id}', [UsersController::class, 'updateAvatar']);
+    Route::post('dash/setconfig', [ConfigController::class, 'setconfig']);
 });
+
+Route::get('dash/config', [ConfigController::class, 'index']);
--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ConfigController;
 use App\Http\Controllers\OptionsController;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\PermissionsController;
-use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\TermsController;
 use App\Http\Controllers\DashboardController;
@@ -30,17 +28,12 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::apiResources([
-        'users'         => UsersController::class,
         'roles'         => RolesController::class,
         'permissions'   => PermissionsController::class,
         'posts'         => PostsController::class,
         'terms'         => TermsController::class
     ]);
-
-    Route::put('user/updatepassword/{id}', [UsersController::class, 'updatePassword']);
-    Route::put('user/updateavatar/{id}', [UsersController::class, 'updateAvatar']);
     Route::get('option/{key}', [OptionsController::class, 'get']);
-    Route::post('setconfig', [ConfigController::class, 'setconfig']);
 
     //dashboard
     Route::get('dashboard/datatable', [DashboardController::class, 'datatable']);
@@ -51,6 +44,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('notifications/mark-all-as-read', [NotificationsController::class, 'markAllAsRead']);
 });
 
-Route::get('config', [ConfigController::class, 'index']);
 
 require __DIR__ . '/api-dash.php';
