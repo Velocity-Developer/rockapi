90e751bbe76ae05b374d06bdc8080bb65d8f3f0f
adityathok <aditya0thok@gmail.com>
2025-04-25 14:22:15 +0700

    create model Webhost

---
+++ app/Models/Webhost.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Webhost extends Model
+{
+    // Nama tabel di database
+    protected $table = 'tb_webhost';
+
+    // Nama primary key yang tidak konvensional
+    protected $primaryKey = 'id_webhost';
+
+    protected $fillable = [
+        'nama_web',
+        'id_paket',
+        'tgl_mulai',
+        'id_server',
+        'id_server2',
+        'space',
+        'space_use',
+        'hp',
+        'telegram',
+        'hpads',
+        'wa',
+        'email',
+        'tgl_exp',
+        'tgl_update',
+        'server_luar',
+        'saldo',
+        'kategori',
+        'waktu',
+        'staff',
+        'via',
+        'konfirmasi_order',
+        'kata_kunci',
+        'jenis_kelamin',
+        'usia',
+    ];
+}
