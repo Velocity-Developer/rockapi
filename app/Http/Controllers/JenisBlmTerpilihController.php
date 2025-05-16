b5166b796ffb804263a5c5f025975e23936f2276
adityathok <aditya0thok@gmail.com>
2025-05-16 15:32:08 +0700

    refactor: jenis_blm_dipilih index

--- app/Http/Controllers/JenisBlmTerpilihController.php
+++ app/Http/Controllers/JenisBlmTerpilihController.php
@@ -2,60 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
 use App\Models\CsMainProject;
+use App\Models\TransaksiKeluar;
 use Carbon\Carbon;
-use App\Services\CsMainProjectTransaksiKeluarService;
 
 class JenisBlmTerpilihController extends Controller
 {
     //index
-    public function index(Request $request, CsMainProjectTransaksiKeluarService $service)
+    public function index(Request $request)
     {
-        $dari       = $request->input('tgl_masuk_start');
-        $sampai      = $request->input('tgl_masuk_end');
+        $dari   = $request->input('tgl_masuk_start');
+        $sampai = $request->input('tgl_masuk_end');
 
-        $get_data = $service->getLaporanTransaksi($dari, $sampai);
-
-        //susun ulang data
-        $data = [];
-        foreach ($get_data as $item) {
-            $tgl_masuk = Carbon::parse($item->tgl_masuk)->format('Y-m-d');
-
-            if ($item->masuk == '0') {
-                $id_transaksi = 'keluar-' . $item->id_transaksi_keluar;
-                $kategori = 'keluar';
-            } else {
-                $id_transaksi = 'masuk-' . $item->id_transaksi_cs_main;
-                $kategori = 'masuk';
-            }
-
-            $data[$tgl_masuk][$id_transaksi] = [
-                'id_transaksi'  => $id_transaksi,
-                'kategori'      => $kategori,
-                'jenis'         => $item->jenis,
-                'nama_web'      => $item->nama_web,
-                'transfer'      => $item->trf,
-                'deskripsi'     => $item->deskripsi,
-                'biaya'         => $item->biaya,
-                'dibayar'       => $item->masuk,
-                'tgl'           => $item->tgl_masuk,
-                'tgl_deadline'  => $item->tgl_deadline,
-                'keluar'        => $item->keluar,
-            ];
+        //jika $dari kosong, gunakan tanggal di awal bulan sekarang
+        if (empty($dari)) {
+            //30 hari terakhir
+            $dari = Carbon::now()->subDays(30)->format('Y-m-d');
+        }
+        //jika $sampai kosong, gunakan tanggal di tanggal sekarang
+        if (empty($sampai)) {
+            $sampai = Carbon::now()->format('Y-m-d');
         }
 
-        $list_keu = [];
-        foreach ($data as $keu_top) {
-            foreach ($keu_top as $key => $keu) {
-                $list_keu[] = $keu;
-            }
-        }
+        //ambil data CsMainProject
+        $dataCsMainProject = CsMainProject::with('webhost', 'bank')
+            ->whereBetween('tgl_masuk', [$dari, $sampai])
+            //->whereDoesntHave('bank') // tidak memiliki relasi ke bank
+            ->get()
+            ->map(function ($item) {
+                // Tambahkan properti virtual 'tanggal' agar bisa di-sort bersamaan
+                $item->tanggal = $item->tgl_masuk;
+                $item->tipe = 'masuk';
+                $item->nama_web = $item->webhost->nama_web;
+                return $item;
+            });
 
-        return response()->json([
-            'raw'       => $get_data,
-            'by_tgl'    => $data,
-            'data'      => $list_keu
-        ]);
+        //ambil data TransaksiKeluar
+        $dataTransaksiKeluar = TransaksiKeluar::with('bank')
+            ->whereBetween('tgl', [$dari, $sampai])
+            //->whereDoesntHave('bank') // tidak memiliki relasi ke bank
+            ->get()
+            ->map(function ($item) {
+                // Tambahkan properti virtual 'tanggal'
+                $item->tanggal = $item->tgl;
+                $item->tipe = 'keluar';
+                return $item;
+            });
+
+        $gabungan = $dataCsMainProject->merge($dataTransaksiKeluar)->sortByDesc('tanggal')->values();
+
+        $perPage = $request->input('per_page') ?? 50; // jumlah item per halaman
+        $page = $request->input('page') ?? 1; // dapatkan nomor halaman dari URL
+
+        $paginated = new LengthAwarePaginator(
+            $gabungan->forPage($page, $perPage),
+            $gabungan->count(), // total item
+            $perPage,
+            $page,
+            [
+                'path' => 'jenis_blm_terpilih',
+                // 'query' => request()->query() // agar pagination mempertahankan query string
+            ]
+        );
+
+        return response()->json($paginated);
     }
 }
