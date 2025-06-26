99a062ab1ff26e5673842727c2b7f7d79e5b47ef
adityathok <aditya0thok@gmail.com>
2025-06-26 08:45:12 +0700

    fix: bug  Webhost create, default waktu

--- app/Http/Controllers/CsMainProjectController.php
+++ app/Http/Controllers/CsMainProjectController.php
@@ -59,7 +59,7 @@ class CsMainProjectController extends Controller
                 'server_luar'       => $request->input('server') && $request->input('server') == '4' ? '0' : '1',
                 'saldo'             => $request->input('saldo'),
                 'kategori'          => '',
-                'waktu'             => date('Y-m-d H:i:s'),
+                'waktu'             => '0000-00-00 00:00:00',
                 'via'               => '',
                 'konfirmasi_order'  => '',
                 'kata_kunci'        => '',
