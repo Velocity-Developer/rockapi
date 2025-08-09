27856e8b66b17cb09ccbc40950253a448624164d
adityathok <aditya0thok@gmail.com>
2025-08-09 14:48:15 +0700

    update: journal table

--- database/migrations/2025_08_01_065424_create_journal_categories_table.php
+++ database/migrations/2025_08_01_065424_create_journal_categories_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('journal_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('role')->nullable();
+            $table->string('icon')->nullable();
             $table->timestamps();
         });
     }
