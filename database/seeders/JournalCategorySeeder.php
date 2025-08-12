0f521e89d8f63c410b3cf73052e37034c49f9d70
adityathok <aditya0thok@gmail.com>
2025-08-12 11:35:54 +0700

    add: seeder JournalCategory

--- database/seeders/JournalCategorySeeder.php
+++ database/seeders/JournalCategorySeeder.php
@@ -14,21 +14,94 @@ class JournalCategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            'Project',
-            'Pengembangan',
-            'Desain',
-            'Support',
-            'Training',
-            'Konsep',
-            'Lainnya',
+            [
+                'name' => 'Project',
+                'description' => 'Project',
+                'icon' => '🔧',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Pengembangan',
+                'description' => 'Pengembangan',
+                'icon' => '🧪',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Desain',
+                'description' => 'Desain',
+                'icon' => '🎨',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Support',
+                'description' => 'Support',
+                'icon' => '🤝',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Training',
+                'description' => 'Training',
+                'icon' => '🎓',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Konsep',
+                'description' => 'Konsep',
+                'icon' => '💡',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Lainnya',
+                'description' => 'Lainnya',
+                'icon' => '🔍',
+                'role' => 'webdeveloper',
+            ],
+            [
+                'name' => 'Panduan',
+                'description' => 'Panduan penggunaan website klien',
+                'icon' => '📣',
+                'role' => 'support',
+            ],
+            [
+                'name' => 'Konsultasi Update',
+                'description' => 'Konsultasi update website klien',
+                'icon' => '📝',
+                'role' => 'support',
+            ],
+            [
+                'name' => 'Pengerjaan Update',
+                'description' => 'Pengerjaan update website klien',
+                'icon' => '📝',
+                'role' => 'support',
+            ],
+            [
+                'name' => 'Trouble',
+                'description' => 'Layanan kendala website klien',
+                'icon' => '🕷',
+                'role' => 'support',
+            ],
+            [
+                'name' => 'Lain-lain',
+                'description' => 'Layanan support lainnya',
+                'icon' => '🦾',
+                'role' => 'support',
+            ],
         ];
 
         foreach ($categories as $category) {
-            JournalCategory::create([
-                'name' => $category,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            //create or update
+            JournalCategory::updateOrCreate(
+                [
+                    'name' => $category['name'],
+                    'role' => $category['role']
+                ],
+                [
+                    'description'   => $category['description'],
+                    'icon'          => $category['icon'],
+                    'created_at'    => now(),
+                    'updated_at'    => now(),
+                ]
+            );
         }
     }
 }
