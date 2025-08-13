f4557f74d32d2676430cf9b829a0abc57447ae92
adityathok <aditya0thok@gmail.com>
2025-08-13 08:30:55 +0700

    add: journal seeder

--- database/seeders/JournalSeeder.php
+++ database/seeders/JournalSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Journal;
+use App\Models\JournalCategory;
+use App\Models\Webhost;
 
 class JournalSeeder extends Seeder
 {
@@ -12,6 +17,42 @@ class JournalSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker = Faker::create();
+
+        //dapatkan semua category
+        $categories = JournalCategory::all();
+
+        //looping user
+        foreach ($categories as $category) {
+            $role = $category->role;
+
+            //get user by role
+            $users = User::role($role)->get();
+
+            foreach ($users as $user) {
+
+                //get random webhost
+                $webhost = Webhost::inRandomOrder()->first();
+
+                //get date, kemarin sampai 1 bulan lalu
+                $random_day_subs = rand(2, 30);
+                $start = $faker->dateTimeBetween('-' . $random_day_subs . ' days', 'now');
+                $end = $faker->dateTimeBetween($start, 'now');
+
+                //create journal
+                Journal::create([
+                    'user_id'               => $user->id,
+                    'journal_category_id'   => $category->id,
+                    'title'                 => $faker->sentence(10),
+                    'description'           => $faker->paragraph(10),
+                    'start'                 => $start,
+                    'end'                   => $end,
+                    'status'                => $faker->randomElement(['ongoing', 'completed', 'cancelled', 'archived']),
+                    'webhost_id'            => $webhost->id_webhost,
+                ]);
+
+                $this->command->info('Create Journal: ' . $user->name . ' - ' . $category->name);
+            }
+        }
     }
 }
