8fc497a390d464689f550526c772325d3021a4e6
adityathok <aditya0thok@gmail.com>
2025-09-16 11:03:30 +0700

    feat(dashboard): add welcome endpoint for webdeveloper projects
    Add new endpoint to get dashboard statistics specifically for webdeveloper projects. The endpoint follows similar logic to existing welcome endpoint but filters by webdeveloper project types. Also refactors date range calculation to use Carbon for better reliability.

--- app/Http/Controllers/Dash/DashboardController.php
+++ app/Http/Controllers/Dash/DashboardController.php
@@ -11,18 +11,33 @@ use Carbon\Carbon;
 class DashboardController extends Controller
 {
 
+    private $jenis_project_webdeveloper = [
+        'Jasa Update Web',
+        'Pembuatan',
+        'Pembuatan apk custom',
+        'Pembuatan Tanpa Domain',
+        'Pembuatan Tanpa Hosting',
+        'Pembuatan Tanpa Domain+Hosting',
+        'Redesign',
+        'Pembuatan web konsep',
+    ];
+
     public function welcome()
     {
         //dapatkan total CsMainProject bulan ini
-        $totalCsMainProject = CsMainProject::whereMonth('tgl_masuk', date('m'))->count();
+        $totalCsMainProject = CsMainProject::whereMonth('tgl_masuk', date('m'))
+            ->whereYear('tgl_masuk', date('Y'))
+            ->count();
 
         //tanggal sekarang
-        $tanggalSekarang = date('d');
+        $now = Carbon::now();
+        $tanggalSekarang = $now->day;
 
-        //dapatkan total CsMainProject bulan lalu, tapi sampai tanggal sekarang
-        $totalCsMainProjectLastMonth = CsMainProject::whereMonth('tgl_masuk', date('m', strtotime('-1 month')))
-            ->whereDay('tgl_masuk', '<=', $tanggalSekarang)
-            ->count();
+        // range bulan lalu
+        $start = $now->copy()->subMonth()->startOfMonth();
+        $end   = $now->copy()->subMonth()->startOfMonth()->addDays($tanggalSekarang - 1);
+
+        $totalCsMainProjectLastMonth = CsMainProject::whereBetween('tgl_masuk', [$start, $end])->count();
 
 
         //hitung persentase
@@ -169,5 +184,36 @@ class DashboardController extends Controller
             ->orderBy('date', 'desc')
             ->paginate(8);
         return response()->json($Posts);
+    }
+
+    public function welcome_webdeveloper()
+    {
+        //dapatkan total CsMainProject bulan ini
+        $totalCsMainProject = CsMainProject::whereMonth('tgl_masuk', date('m'))
+            ->whereYear('tgl_masuk', date('Y'))
+            ->whereIn('jenis', $this->jenis_project_webdeveloper)
+            ->count();
+
+        //tanggal sekarang
+        $now = Carbon::now();
+        $tanggalSekarang = $now->day;
+
+        // range bulan lalu
+        $start = $now->copy()->subMonth()->startOfMonth();
+        $end   = $now->copy()->subMonth()->startOfMonth()->addDays($tanggalSekarang - 1);
+
+        $totalCsMainProjectLastMonth = CsMainProject::whereBetween('tgl_masuk', [$start, $end])
+            ->whereIn('jenis', $this->jenis_project_webdeveloper)
+            ->count();
+
+        //hitung persentase
+        $persentase = ($totalCsMainProject - $totalCsMainProjectLastMonth) / $totalCsMainProjectLastMonth * 100;
+
+        return response()->json([
+            'total_project_bulanini' => $totalCsMainProject,
+            'total_project_bulanlalu' => $totalCsMainProjectLastMonth,
+            'perform' => round($persentase, 2),
+            'date' => date('m'),
+        ]);
     }
 }
--- routes/api-dash.php
+++ routes/api-dash.php
@@ -24,6 +24,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('dashboard/chart_tahunini', [DashboardController::class, 'chart_tahunini']);
     Route::get('dashboard/chart_hariini', [DashboardController::class, 'chart_hariini']);
     Route::get('dashboard/datatable', [DashboardController::class, 'datatable']);
+    Route::get('dashboard/welcome_webdeveloper', [DashboardController::class, 'welcome_webdeveloper']);
 
     //options
     Route::get('option/{key}', [OptionsController::class, 'get']);
