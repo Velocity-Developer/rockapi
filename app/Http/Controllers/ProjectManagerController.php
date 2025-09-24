8b807d82688fe6c541de97ed21388b41fcd29d69
adityathok <aditya0thok@gmail.com>
2025-09-24 14:41:09 +0700

    feat(ProjectManagerController): add filtering options for project listing
    Implement date range, webhost name, package, type, and description filters to enhance project search functionality

--- app/Http/Controllers/ProjectManagerController.php
+++ app/Http/Controllers/ProjectManagerController.php
@@ -21,6 +21,41 @@ class ProjectManagerController extends Controller
             'wm_project.user:id,name,avatar',
         );
 
+        // Apply date filter if both start and end dates are provided
+        $tgl_masuk_start    = $request->input('tgl_masuk_start');
+        $tgl_masuk_end      = $request->input('tgl_masuk_end');
+        if ($tgl_masuk_start && $tgl_masuk_end) {
+            $query->whereBetween('tgl_masuk', [$tgl_masuk_start, $tgl_masuk_end]);
+        }
+
+        //filter by webhost.nama_web
+        $nama_web = $request->input('nama_web');
+        if ($nama_web) {
+            $query->whereHas('webhost', function ($query) use ($nama_web) {
+                $query->where('nama_web', 'like', '%' . $nama_web . '%');
+            });
+        }
+
+        //filter by webhost.paket.nama_paket
+        $id_paket = $request->input('paket');
+        if ($id_paket) {
+            $query->whereHas('webhost.paket', function ($query) use ($id_paket) {
+                $query->where('id_paket', $id_paket);
+            });
+        }
+
+        //filter by jenis
+        $jenis = $request->input('jenis');
+        if ($jenis) {
+            $query->where('jenis', 'like', '%' . $jenis . '%');
+        }
+
+        //filter by deskripsi
+        $deskripsi = $request->input('deskripsi');
+        if ($deskripsi) {
+            $query->where('deskripsi', 'like', '%' . $deskripsi . '%');
+        }
+
         //order by
         $order_by           = $request->input('order_by', 'tgl_deadline');
         $order              = $request->input('order', 'desc');
