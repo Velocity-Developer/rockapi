f230f69175bcd2a95786afa1c5921ae42f095cbc
adityathok <aditya0thok@gmail.com>
2025-09-27 14:01:32 +0700

    feat(models): add CsMainProjectInfo model and migration

---
+++ app/Models/CsMainProjectInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CsMainProjectInfo extends Model
+{
+    protected $fillable = [
+        'cs_main_project_id',
+        'author_id',
+        'jenis_project',
+    ];
+
+    //relasi ke CsMainProject
+    public function cs_main_project()
+    {
+        return $this->belongsTo(CsMainProject::class, 'cs_main_project_id');
+    }
+
+    //relasi ke User
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
