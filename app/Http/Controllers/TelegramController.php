afdd4389756d7f4268a2f1f141c46696eaf147f0
adityathok <aditya0thok@gmail.com>
2025-11-11 13:16:49 +0700

    feat: telegram webhook with token

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class TelegramController extends Controller
 {
@@ -10,12 +11,22 @@ class TelegramController extends Controller
     public function webhook(Request $request)
     {
         $message = $request->input('message');
-        if (!$message) return response()->json(['ok' => false]);
+        if (!$message) return response()->json(['ok' => false, 'message' => 'message not found']);
 
         $telegramId = $message['chat']['id'];
 
-        //simpan ke user login
-        $user = auth()->user();
+        //check token
+        $token = $request->input('token');
+        if (!$token) return response()->json(['ok' => false, 'message' => 'token not found']);
+
+        //get user id from cache
+        $userId = cache()->get($token);
+        if (!$userId) return response()->json(['ok' => false, 'message' => 'token not valid']);
+
+        //get user from db
+        $user = User::find($userId);
+        if (!$user) return response()->json(['ok' => false, 'message' => 'user not found']);
+
         $user->telegram_id = $telegramId;
         $user->save();
 
@@ -30,8 +41,13 @@ class TelegramController extends Controller
 
         $webhook_url = '';
         if (!$telegramId) {
+
+            //create cache token for 1 hour
+            $token = 'telegram_token_' . uniqid();
+            cache()->put($token, $user->id, 60 * 60);
+
             $TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN', 'XXX');
-            $webhook_url = 'https://api.telegram.org/bot' . $TELEGRAM_BOT_TOKEN . '/setWebhook?url=' . config('app.url') . '/api/telegram/webhook';
+            $webhook_url = 'https://api.telegram.org/bot' . $TELEGRAM_BOT_TOKEN . '/setWebhook?url=' . config('app.url') . '/api/telegram/webhook?token=' . $token;
         }
 
         return response()->json([
