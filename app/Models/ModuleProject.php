6ee3ee57af4176a253396984d6d6c61d27936a9a
adityathok <aditya0thok@gmail.com>
2025-11-20 09:56:58 +0700

    feat: Modul ModuleProject

---
+++ app/Models/ModuleProject.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ModuleProject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'version',
+        'github_url',
+        'download_url',
+        'type'
+    ];
+
+    protected $casts = [
+        'type' => 'string'
+    ];
+
+    public function getTypeLabel(): string
+    {
+        return match($this->type) {
+            'theme' => 'Theme',
+            'plugin' => 'Plugin',
+            'child_theme' => 'Child Theme',
+            default => 'Unknown'
+        };
+    }
+}
