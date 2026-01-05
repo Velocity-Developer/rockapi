5e1e70bb81c3ac21957075c565e3cbd5fd2edb8e
adityathok <aditya0thok@gmail.com>
2026-01-05 16:41:53 +0700

    refactor(routes): update rekap-form-konversi-ads endpoint paths
    Changed endpoint paths from 'rekap-form/konversi-ads' to 'rekap-form-konversi-ads' for consistency and better URL readability

--- routes/api-public.php
+++ routes/api-public.php
@@ -7,6 +7,6 @@ use App\Http\Controllers\RekapFormController;
 Route::middleware(['api_public_verify'])->prefix('public')->group(function () {
     Route::post('rekap-form', [RekapFormController::class, 'store']);
     Route::get('rekap-form/{id}', [RekapFormController::class, 'show']);
-    Route::get('rekap-form/konversi-ads', [RekapFormController::class, 'get_konversi_ads']);
-    Route::put('rekap-form/konversi-ads', [RekapFormController::class, 'update_cek_konversi_ads']);
+    Route::get('rekap-form-konversi-ads', [RekapFormController::class, 'get_konversi_ads']);
+    Route::put('rekap-form-konversi-ads', [RekapFormController::class, 'update_cek_konversi_ads']);
 });
