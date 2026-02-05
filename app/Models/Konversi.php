befd97200a49ee66dc1b151ea83dcd6f71d463b4
adityathok <aditya0thok@gmail.com>
2026-02-05 14:01:49 +0700

    docs: desc model Konversi

--- app/Models/Konversi.php
+++ app/Models/Konversi.php
@@ -4,6 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * ini adalah model Konversi.
+ * menggabungkan data konversi dari tabel 'tb_konversi','tb_konversi_display','tb_konversi_wa5','tb_konversi_organik'.
+ * dalam satu tabel.
+ */
+
 class Konversi extends Model
 {
     protected $table = 'konversi';
