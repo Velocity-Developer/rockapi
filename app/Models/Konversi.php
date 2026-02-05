9a46405ab838e6dc9d4e90e8ab920b43c78216eb
adityathok <aditya0thok@gmail.com>
2026-02-05 13:41:47 +0700

    feat: Model Konversi

---
+++ app/Models/Konversi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Konversi extends Model
+{
+    protected $table = 'konversi';
+
+    protected $fillable = [
+        'tanggal',
+        'value',
+        'kategori',
+    ];
+}
