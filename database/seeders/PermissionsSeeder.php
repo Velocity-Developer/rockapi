4fbc2085249f7b0915ed89a55c2ec89cd773fba2
adityathok <aditya0thok@gmail.com>
2026-02-20 13:42:20 +0700

    feat: add initial permissions seeder with core application roles
    Introduce a seeder to populate the database with essential permissions for user management, content creation, billing, and team support functionalities. The seeder checks for existing permissions to avoid duplicates.

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // daftar permissions
+        $permissions = [
+            'page-dashboard',
+            'page-users',
+            'view-other-user',
+            'edit-other-user',
+            'create-other-user',
+            'edit-user',
+            'delete-user',
+            'edit-settings',
+            'create-post',
+            'edit-post',
+            'delete-post',
+            'edit-term',
+            'add-billing',
+            'timsupport-journal-perform-tim',
+            'timads-journal-perform-tim',
+            'edit-cuti',
+        ];
+
+        foreach ($permissions as $permission) {
+            // check permission
+            if (! Permission::where('name', $permission)->exists()) {
+                Permission::create(['name' => $permission]);
+                $this->command->info('Permission created: ' . $permission);
+            }
+        }
+    }
+}
