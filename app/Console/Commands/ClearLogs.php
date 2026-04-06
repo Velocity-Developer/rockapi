ec7f8e94e15ff22887694c012c3dfdd493744dc9
adityathok <aditya0thok@gmail.com>
2026-04-06 11:11:49 +0700

    feat(command): add 'clear:logs'

---
+++ app/Console/Commands/ClearLogs.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:logs';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear all logs';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $files = glob(storage_path('logs/*.log'));
+        foreach ($files as $file) {
+            file_put_contents($file, '');
+        }
+        $this->info('Logs have been cleared!');
+    }
+}
