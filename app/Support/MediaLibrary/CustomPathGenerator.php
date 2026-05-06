075d0a4fe617db254a4b24e665424e4b035e1cde
adityathok <aditya0thok@gmail.com>
2026-05-06 13:55:09 +0700

    feat: CustomPathGenerator pastikan folder slug

--- app/Support/MediaLibrary/CustomPathGenerator.php
+++ app/Support/MediaLibrary/CustomPathGenerator.php
@@ -4,6 +4,7 @@ namespace App\Support\MediaLibrary;
 
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
+use Illuminate\Support\Str;
 
 class CustomPathGenerator implements PathGenerator
 {
@@ -11,7 +12,7 @@ class CustomPathGenerator implements PathGenerator
     {
         $year = $media->created_at->format('Y');
         $month = $media->created_at->format('m');
-        $model = class_basename($media->model_type);
+        $model = Str::slug(class_basename($media->model_type));
 
         return "{$model}/{$year}-{$month}/{$media->id}/";
     }
